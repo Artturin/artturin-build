225627a915189d9416c36b8521a39c2c0215a06a
Artturin <Artturin@artturin.com>
2021-07-27 11:28:13 +0300

    c

--- default.nix
+++ default.nix
@@ -5,7 +5,9 @@
 {
   lib = import ./lib { inherit pkgs; }; # functions
   tinycc = pkgs.callPackage ./pkgs/tinycc { };
-  tinycc-unstable = pkgs.callPackage ./pkgs/tinycc/unstable.nix { };
+  tinycc-unstable = pkgs.callPackage ./pkgs/tinycc/unstable.nix { 
+      inherit (darwin.apple_sdk.frameworks) CoreServices;
+  };
 
 
 }
