1fe69e9c6ab8a7a4121fc7a963022b94fc2f6f8c
Artturin <Artturin@artturin.com>
2021-07-27 11:32:11 +0300

    c

--- default.nix
+++ default.nix
@@ -6,8 +6,8 @@
   lib = import ./lib { inherit pkgs; }; # functions
   tinycc = pkgs.callPackage ./pkgs/tinycc { };
   tinycc-unstable = pkgs.callPackage ./pkgs/tinycc/unstable.nix { 
-      inherit (darwin) DarwinTools cctools;
-      inherit (darwin.apple_sdk.frameworks) CoreServices CoreFoundation;
+      inherit (pkgs.darwin) DarwinTools cctools;
+      inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices CoreFoundation;
   };
 
 
