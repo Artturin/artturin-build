e72ab40ee8efbc69e7c11a7d296ee04e5060eb1d
Artturin <Artturin@artturin.com>
2021-07-30 19:28:59 +0300

    c

--- default.nix
+++ default.nix
@@ -4,11 +4,28 @@
 
 {
   lib = import ./lib { inherit pkgs; }; # functions
-  tinycc = pkgs.callPackage ./pkgs/tinycc { };
-  tinycc-unstable = pkgs.callPackage ./pkgs/tinycc/unstable.nix { 
-      inherit (pkgs.darwin) DarwinTools cctools;
-      #inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices CoreFoundation;
-  };
+#  tinycc = pkgs.callPackage ./pkgs/tinycc { };
+#  tinycc-unstable = pkgs.callPackage ./pkgs/tinycc/unstable.nix { 
+#      inherit (pkgs.darwin) DarwinTools cctools;
+#      #inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices CoreFoundation;
+#  };
+
+  inherit (pkgs.callPackages ./pkgs/crystal {
+    llvmPackages = pkgs.llvmPackages_10;
+  })
+    #crystal_0_33
+    #crystal_0_34
+    #crystal_0_35
+    #crystal_0_36
+    crystal_1_0
+    crystal;
+
+  inherit (pkgs.callPackage ./pkgs/shards { })
+    shards_0_11
+    shards_0_15
+    shards;
+
+  mint = pkgs.callPackage ./pkgs/mint { };
 
 
 }
